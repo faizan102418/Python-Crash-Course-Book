60bc39f1b93e41ac11fe9dfde9f076ec6fd76962
faizan102418 <m.faizansajid102418@gmail.com>
2024-10-19 22:37:02 +0500

    Create chapter_03.py

---
+++ Code Work/chapter_03.py
@@ -0,0 +1,8 @@
+names = ["Tariq","Mazhar","Ali","Razz"]
+print("Hello", names[0])
+
+transportation = ["Car","Jet","Yatch"]
+print("I would like to go through" , transportation[0])
+print("I would like to go through" , transportation[1])
+print("I would like to go through" , transportation[2])
+# print("I would like to go through" , transportation[3])
