396b6979ed8d41528dcdeec46d893076a6984276
faizan102418 <m.faizansajid102418@gmail.com>
2024-10-19 22:45:32 +0500

    Update chapter_03.py

--- Code Work/chapter_03.py
+++ Code Work/chapter_03.py
@@ -6,3 +6,6 @@ print("I would like to go through" , transportation[0])
 print("I would like to go through" , transportation[1])
 print("I would like to go through" , transportation[2])
 # print("I would like to go through" , transportation[3])
+transportation[0] = "Bugatti"
+print(transportation)
+print("I would like to go through" , transportation[0])
