454b88652394dbca90bd6c650588dd2ebf7b916b
faizan102418 <m.faizansajid102418@gmail.com>
2024-10-24 18:32:10 +0500

    Completed Chapter 3
    Work with list

--- Code Work/chapter_03.py
+++ Code Work/chapter_03.py
@@ -1,11 +1,107 @@
-names = ["Tariq","Mazhar","Ali","Razz"]
-print("Hello", names[0])
+#names=["Tariq","Mazhar","Ali","Razz"]
+#print("Hello",names[0])
 
-transportation = ["Car","Jet","Yatch"]
-print("I would like to go through" , transportation[0])
-print("I would like to go through" , transportation[1])
-print("I would like to go through" , transportation[2])
-# print("I would like to go through" , transportation[3])
-transportation[0] = "Bugatti"
-print(transportation)
-print("I would like to go through" , transportation[0])
+#transportation=["Car","Jet","Yatch"]
+#print("Iwouldliketogothrough",transportation[0])
+#print("Iwouldliketogothrough",transportation[1])
+#print("Iwouldliketogothrough",transportation[2])
+##print("Iwouldliketogothrough",transportation[3])
+#transportation[0]="Bugatti"
+#print(transportation)
+#print("Iwouldliketogothrough",transportation[0])
+
+#motorcycles=["honda","yamaha","suzuki"]
+#print(motorcycles)
+#motorcycles.append("ducati")
+#print(motorcycles)
+
+#motorcycles=[]
+#motorcycles.append('honda')
+#motorcycles.append('yamaha')
+#motorcycles.append('suzuki')
+##print(motorcycles)
+
+#motorcycles=['honda','yamaha','suzuki']
+#motorcycles.insert(2,'ducati')
+#print(motorcycles)
+#delmotorcycles[0]
+#print(motorcycles)
+
+#motorcycles=['honda','yamaha','suzuki']
+#print(motorcycles)
+#popped_motorcycle=motorcycles.pop()
+#print(motorcycles)
+#print(popped_motorcycle)
+
+#motorcycles=['honda','yamaha','suzuki','ducati']
+#print(motorcycles)
+#motorcycles.remove('honda')
+#print(motorcycles)
+
+
+#motorcycles=['honda','yamaha','suzuki','ducati']
+#print(motorcycles)
+#too_expensive='ducati'
+#motorcycles.remove(too_expensive)
+#print(motorcycles)
+#print(f"\nA{too_expensive.title()}istooexpensiveforme.")
+
+
+#to_invite=["Tariq","Mazhar","Khan"]
+
+#for_into_invite:
+#print(f"Hello{_},TommarowisBirthdayandyourinvitedtocelebratetogether::")
+#to_invite[0]="Ali"
+#not_here=to_invite[0]
+#print(f"Hey{not_here}againiaminvitingyou")
+#to_invite.insert(0,"Asad")
+#to_invite.insert(3,"Rauf")
+#to_invite.append("Raz")
+#forpersoninto_invite:
+#print(f"Hello{person},Tommarowisdinnerpartyfrommysideandyourinvitedtocelebratetogether::")
+
+#print("Iwillinviteonlytwopersons's")
+
+#last_person=to_invite.pop()
+#print(f"Sorry{last_person}")
+#last_per=to_invite.pop(3)
+#print(f"Sorry{last_per}")
+#last_pe=to_invite.pop(2)
+#print(f"sorry{last_pe}")
+#print(to_invite)
+#forremaininto_invite:
+#print(f"Yourarestillinvited{remain}")
+
+#delto_invite[0:3]
+#print(to_invite)
+
+#cars=['bmw','audi','toyota','subaru']
+#cars.sort(reverse=False)
+#print(cars)
+
+#cars=['bmw','audi','toyota','subaru']
+#print("Hereistheoriginallist:")
+#print(cars)
+#print("\nHereisthesortedlist:")
+#print(sorted(cars))
+#print("\nHereistheoriginallistagain:")
+#print(cars)
+
+# cars=['bmw','audi','toyota','subaru']
+# print(cars)
+# cars.reverse()
+# print(cars)
+
+
+# TODO : Try Yourself Exercises
+
+wish_to_visit = ["Makkah" , "Madina" , "Dubai" , "Germany" ,"Swizerland"]
+# print(wish_to_visit)
+# wish_to_visit.reverse()
+# print(wish_to_visit)
+# wish_to_visit.reverse()
+# print(wish_to_visit)
+
+wish_to_visit.sort(reverse=True)
+print(wish_to_visit)
+print(wish_to_visit[-1])
