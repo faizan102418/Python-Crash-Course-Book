dad1c0bb854e118e5a25787bde72b20c300bb808
faizan102418 <m.faizansajid102418@gmail.com>
2024-10-30 11:25:11 +0500

    Half done
    In this chapter we explored "if - conditons"

---
+++ Code Work/chapter_05.py
@@ -0,0 +1,53 @@
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+# for car in cars:
+#     if car == 'bmw':
+# print(car.upper())
+#     else:
+# print(car.title())
+
+# requested_topping = 'mushrooms'
+# if requested_topping != 'anchovies':
+# print("Hold the anchovies!")
+# else:
+# print("anchovies!")
+
+# answer = 17
+# if answer != 42:
+# print("That is not the correct answer. Please try again!")
+
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'marie'
+# if user not in banned_users:
+# print(f"{user.title()}, you can post a response if you wish.")
+
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+# print("\nIs car != 'subaru'? I predict False.")
+# print(car != 'subaru')
+# print("\nIs car != 'audi'? I predict True.")
+# print(car != 'audi')    
+# print("\nIs car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+
+# car = 'subaru'
+# print(car == "SUBARU")
+# print(car == "SUBARU".lower())
+
+
+num = 10
+num_1 = 20
+# print(num > num_1)
+# print(num < num_1)
+# print(num >= num_1)
+# print(num <= num_1)
+# print(num == num_1)
+# print(num != num_1)
+
+
+lst = ["Ali" , "Mazhar" , "Tariq"]
+print("Tariq" in lst)
+print("Tariq" not in lst)
+print("Zain"  in lst)
