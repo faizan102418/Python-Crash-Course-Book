aaf41725744b4a19a428a8cac5957de44a937af1
faizan102418 <m.faizansajid102418@gmail.com>
2024-10-31 08:53:29 +0500

    Completed

--- Code Work/chapter_05.py
+++ Code Work/chapter_05.py
@@ -47,7 +47,130 @@ num_1 = 20
 # print(num != num_1)
 
 
-lst = ["Ali" , "Mazhar" , "Tariq"]
-print("Tariq" in lst)
-print("Tariq" not in lst)
-print("Zain"  in lst)
+# lst = ["Ali" , "Mazhar" , "Tariq"]
+# print("Tariq" in lst)
+# print("Tariq" not in lst)
+# print("Zain"  in lst)
+
+# age = 19
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+
+
+# age = 17
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered to vote yet?")
+# else:
+#     print("Sorry, you are too young to vote.")
+#     print("Please register to vote as soon as you turn 18!")
+
+
+# age = 12
+# if age < 4:
+#     print("Your admission cost is $0.")
+# elif age < 18:
+#     print("Your admission cost is $25.")
+# else:
+#     print("Your admission cost is $40.")
+
+# alien_color = ['green', 'yellow', 'red']
+
+# if 'green' in alien_color:
+#     print("You just earned 5 points!")
+# elif 'yellow' in alien_color:
+#     print("You just earned 10 points!")
+# elif 'red' in alien_color:
+# else:
+#     print("You just earned 10 points!")
+
+# age = 1
+# if age < 2:
+#     print("The person is a baby.")
+# elif age >= 2 and age < 4:
+#     print("The person is a toddler.")
+# elif age >= 4 and age < 13:
+#     print("The person is a kid.")
+# elif age >= 13 and age < 20:
+#     print("The person is teenager.")
+# elif age >= 20 and age < 65:
+#     print("The person is an adult.")
+# else:
+#     print("The person is an elder.")
+
+
+# favorite_fruits = ["apple", "banana", "cherry"]
+# if "apple" in favorite_fruits:
+#     print("You really like apple!")
+# if "banana" in favorite_fruits:
+#     print("You really like banana!")
+# if "cherry" in favorite_fruits:
+#     print("You really like cherry!")
+# if "orange" in favorite_fruits:
+#     print("You really like orange!")
+# if "mango" in favorite_fruits:
+#     print("You really like mango!")
+    
+    
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# for requested_topping in requested_toppings:
+#     if requested_topping == 'green peppers':
+#         print("Sorry, we are out of green peppers right now.")
+#     else:
+#         print(f"Adding {requested_topping}.")
+# print("\nFinished making your pizza!")
+
+# requested_toppings = ["green"]
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         if requested_topping == 'green':
+#             print("Sorry, we are out of green peppers right now.")
+#         else:
+#             print(f"Adding {requested_topping}.")
+#     print("\nFinished making your pizza!")
+# else:
+#     print("Are you sure you want a plain pizza?")
+
+# available_toppings = ['mushrooms', 'olives', 'green peppers',
+#                       'pepperoni', 'pineapple', 'extra cheese']
+# requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+# for requested_topping in requested_toppings:
+#      if requested_topping in available_toppings:
+#         print(f"Adding {requested_topping}.")
+#      else:
+#         print(f"Sorry, we don't have {requested_topping}.")
+# print("\nFinished making your pizza!")
+
+
+# user = ["admin","Zain"]
+# if user:
+#     for user_name in user:
+#         if user_name == "admin":
+#             print("Hello admin, would you like to see a status report?")
+#         else:
+#             print(f"Hello {user_name}, thank you for logging in again.")
+# else:
+#     print("We need to find some users!")        
+
+# current_user = ["Zain","Tariq","Faizan","Mazhar","Ali"]
+
+# new_user = ["Zain","Tariq","Khan","Jan","Ayela Jani"]
+
+# for user in new_user:
+#     if user in current_user:
+#         print(f"Hello {user} you need to enter a new username")
+#     else:
+#         print(f"Hello {user} you can use this username")  
+
+# ls = list(range(1,100))
+
+# for number in ls:
+#     if number == 1:
+#         print(f"{number}st")
+#     elif number == 2:
+#         print(f"{number}nd")
+#     elif number == 3:
+#         print(f"{number}rd")
+#     else:
+#         print(f"{number}th")
